08d972ccb2f05d0758bfd71135a2407337caa3b3
grollier <chaliv203@gmail.com>
2018-04-06 11:31:56 +0900

    Commiting tha project base

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,7 +11,5 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]

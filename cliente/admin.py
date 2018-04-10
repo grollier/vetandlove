b462ba28a771474401c5995fa20e9f0dd146f096
grollier <chaliv203@gmail.com>
2018-04-10 14:21:35 +0900

    error in clientes when creating duplicate DPI, fixed

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from django.db import models
-from models import *
+from .models import *
 # Register your models here.
 myModels = [Cliente]
 admin.site.register(myModels)
--- cliente/models/cliente.py
+++ cliente/models/cliente.py
@@ -10,10 +10,8 @@ class Cliente(models.Model):
     apellido = models.CharField('Apellido', max_length=50)
     dpi = models.CharField('DPI', max_length=13, unique=True)
     fechaNacimiento = models.DateField()
-    fechaCreacion = models.DateTimeField('publicado en')
-
-    def was_published_recently(self):
-        return self.fechaCreacion >= timezon.now() - datetime.timedelta(days=1)
+    fechaCreacion = models.DateTimeField('publicado en', auto_now=True)
     
+
     def __str__(self):
         return self.nombre

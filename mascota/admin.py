5100732a0435dce1477a38e25fc1e2b7cbc935c7
grollier <chaliv203@gmail.com>
2018-04-10 14:06:32 +0900

    modifying admin site, now everithing can be created from pets view

--- mascota/admin.py
+++ mascota/admin.py
@@ -4,5 +4,5 @@ from django.db import models
 from .models import *
 
 # Register your models here.
-myModels = [Mascota, Especie,]
+myModels = [Mascota]
 admin.site.register(myModels)
--- mascota/models/especie.py
+++ mascota/models/especie.py
@@ -10,7 +10,9 @@ class Especie(models.Model):
        ('O', 'OTRO'),      
     )
     tipoEspecie = models.CharField(max_length=1, choices=TIPO_ESPECIE)
-
+    
+    def __str__(self):
+        return "%s " % self.tipoEspecie
 class Kennel(models.Model):
     kennelId = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=128)

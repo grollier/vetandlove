f548f9f30d0a3007a93e92f58e70075a96467406
grollier <chaliv203@gmail.com>
2018-06-25 00:15:44 -0600

    working around fixes

--- ordenDeTrabajo/models/ordenT.py
+++ ordenDeTrabajo/models/ordenT.py
@@ -3,7 +3,6 @@ from django import forms
 from django.utils import timezone
 
 from cliente.models import Cliente
-
 #Create your models here. 
 class Orden(models.Model):
     ordenId = models.AutoField(primary_key=True)
@@ -15,10 +14,6 @@ class Orden(models.Model):
     observaciones = models.TextField('Observaciones',max_length=500, blank=True, null=True)
     fechaCreacion = models.DateTimeField('Creada_en', auto_now_add=True)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='orden_cliente', default=False)
-    
-    def hora_aproximada(self):
-        hora_entrega = self.horaRecepcion
-        return hora_entrega + datetime.timedelta(hours=2)
 
     def _str__(self):
         return "%s " % (self.nombreOrden, self.ordenId)
